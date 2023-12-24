3fc6fa2c26a189209c91ae3d497301afce2892c1
Joon Lee <joon.lee727@gmail.com>
2023-12-24 00:41:33 -0500

    Fix zsh config for nvm

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -166,10 +166,9 @@
       initExtra = ''
         [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
         export PATH=$PATH:/usr/local/go/bin
-        
-        # nvm config, this loads nvm
-        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
       '';
       plugins = [
         {
